f2bec49cdbe202d09dd94b8f4a8246411b5ff4ec
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-11 21:45:48 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maurício Costa on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
